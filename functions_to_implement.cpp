55396aa601b8a6749aec8fff5983495710029e64
Matthieu251299 <maro3214@colorado.edu>
2022-09-20 17:49:44 +0000

    function changes

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -18,7 +18,14 @@ std::vector<int> MatchVectors(std::vector<int> a, std::vector<int> b);
 
 // divides an input integer by 2 until it is impossible to do so, then returns the final number.
 // (16 = 2 * 2 * 2 * 2 * 1 -> 1, 7 -> 7, 26 = 2 * 13 -> 13, 52 = 2 * 2 * 13 -> 13)
-int RemoveTwos(int original);
+int RemoveTwos(int original)
+{
+	for (int i = 0; i<original; i++){
+		if ( (original % 2) ==0){
+			original /= 2;
+		}else {return original;}
+	}
+}
 
 // takes a vector of integers and removes all elements evenly divisible by the passed in int
 std::vector<int> MultiplesFilter(std::vector<int>, int divides_by);
