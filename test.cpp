bdf7cd5ec65646f982347fb4434d755c379ad21a
Matthieu251299 <maro3214@colorado.edu>
2022-09-20 17:56:45 +0000

    test.cpp changes2

--- test.cpp
+++ test.cpp
@@ -3,8 +3,8 @@
 #include "functions_to_implement.cpp"
 
 TEST_CASE ("Remove Twos", "[removeTows]"){
-	CHECK(RemoveTwos(0)==0)
-	CHECK(RemoveTwos(1)==1)
-	CHECK(Removetwos(2)==1)
-	CHECK(Remove(7)==7)
+	CHECK(RemoveTwos(0)==0);
+	CHECK(RemoveTwos(1)==1);
+	CHECK(RemoveTwos(2)==1);
+	CHECK(RemoveTwos(7)==7);
 }
